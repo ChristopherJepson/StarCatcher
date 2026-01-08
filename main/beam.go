6b561b2c8bf1003c5dfca11620c8d0ffb438a0f0
ChristopherJepson <204491359+ChristopherJepson@users.noreply.github.com>
2026-01-08 11:03:40 -0500

    Feature Enhancements
    Added a limited use tractor beam feature.
    Added UI features for the tractor beam.
    Added logic to reset beam ammunition when restarting game,

--- main/beam.go
+++ main/beam.go
@@ -1,25 +1,17 @@
 components {
-  id: "player"
-  component: "/main/player.script"
+  id: "beam"
+  component: "/main/beam.script"
 }
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "default_animation: \"Spaceship\"\n"
+  data: "default_animation: \"beam\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "textures {\n"
   "  sampler: \"texture_sampler\"\n"
   "  texture: \"/main/game.atlas\"\n"
   "}\n"
   ""
-  position {
-    x: 2.0
-    y: -3.0
-  }
-  scale {
-    x: 0.101724
-    y: 0.113524
-  }
 }
 embedded_components {
   id: "collisionobject"
@@ -30,29 +22,19 @@ embedded_components {
   "restitution: 0.5\n"
   "group: \"player\"\n"
   "mask: \"star\"\n"
-  "mask: \"hazard\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
   "    position {\n"
-  "      x: 3.0\n"
-  "      y: -4.0\n"
   "    }\n"
   "    rotation {\n"
   "    }\n"
   "    index: 0\n"
   "    count: 3\n"
-  "    id: \"Player Basket\"\n"
   "  }\n"
-  "  data: 81.52117\n"
-  "  data: 95.9189\n"
-  "  data: 10.0\n"
+  "  data: 4.311677\n"
+  "  data: 29.996267\n"
+  "  data: 11.6\n"
   "}\n"
-  ""
-}
-embedded_components {
-  id: "beam_factory"
-  type: "factory"
-  data: "prototype: \"/main/beam.go\"\n"
   ""
 }
